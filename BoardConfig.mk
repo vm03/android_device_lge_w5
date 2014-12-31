35d053d20b415be9f058a1a3ab81c81cd2351e19
vm03 <vm03@yandex.ru>
2014-12-31 12:24:12 +0300

    camera cm12 tanks to Quarx

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,7 @@ TARGET_KERNEL_CONFIG := w5_global_com_defconfig
 BOARD_CUSTOM_BOOTIMG_MK := device/lge/w5/mkbootimg.mk
 
 TARGET_LIBINIT_DEFINES_FILE := device/lge/w5/init/init_w5.c
+TARGET_SPECIFIC_HEADER_PATH += device/lge/w5ds/include
 
 DEVICE_RESOLUTION := 480x800
 
