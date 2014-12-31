277e490c665b8de8ea95d37173c9bae65d0968d8
vm03 <vm03@yandex.ru>
2014-12-31 11:48:55 +0300

    add msm323

--- init/init_w5.c
+++ init/init_w5.c
@@ -98,6 +98,13 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.fingerprint", "lge/w5ds_global_com-user/w5ds:4.4.2/KOT49I.D41510c/D41510c.1393916607:user/release-keys");
                 property_set("persist.radio.multisim.config", "dsds");
         property_set("telephony.lteOnCdmaDevice", "0");
+    } else if (strncmp(serial, "LGMS323", 7) == 0) {
+        property_set("ro.product.device", "w5");
+        property_set("ro.product.model", "LG-MS323");
+        property_set("ro.build.description", "w5_global_com-user 4.4.2 KOT49I.D41510c D41510c.1393916607 release-keys");
+        property_set("ro.build.fingerprint", "lge/w5_global_com-user/w5ds:4.4.2/KOT49I.D41510c/D41510c.1393916607:user/release-keys");
+                property_set("persist.radio.multisim.config", "");
+        property_set("telephony.lteOnCdmaDevice", "0");
     } else {
         /* XXX */
         property_set("ro.product.device", "w5");
