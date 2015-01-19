bcbb55c19b5c27ba0f7a66c3a415473070450ed3
vm03 <vm03@yandex.ru>
2015-01-19 13:16:55 +0300

    add keys for MS323

--- device_w5.mk
+++ device_w5.mk
@@ -47,7 +47,8 @@ PRODUCT_COPY_FILES += \
     device/lge/w5/prebuilt/etc/init.d/10nfc_checker:system/etc/init.d/10nfc_checker \
     device/lge/w5/prebuilt/etc/init.d/11keys_checker:system/etc/init.d/11keys_checker \
     device/lge/w5/prebuilt/usr/keylayout/Generic-D320.kl:system/usr/keylayout/Generic-D320.kl \
-    device/lge/w5/prebuilt/usr/keylayout/Generic-D325.kl:system/usr/keylayout/Generic-D325.kl
+    device/lge/w5/prebuilt/usr/keylayout/Generic-D325.kl:system/usr/keylayout/Generic-D325.kl \
+    device/lge/w5/prebuilt/usr/keylayout/Generic-MS323.kl:system/usr/keylayout/Generic-MS323.kl
 
 PRODUCT_LOCALES := en_US
 PRODUCT_LOCALES += hdpi
