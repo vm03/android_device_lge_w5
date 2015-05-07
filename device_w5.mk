9bfbda5c90c7ddba6fceeea329c9ead49b23b920
vm03 <vm03@yandex.ru>
2015-05-07 12:04:36 +0300

    move thermal-engine-8610.conf from common add lcd to thermal-engine-8610.conf
    Change-Id: I76b2ffc3c071174f68b2b423d124c0a9e3485bcd

--- device_w5.mk
+++ device_w5.mk
@@ -37,6 +37,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += \
     device/lge/w5/mixer_paths.xml:system/etc/mixer_paths.xml \
+    device/lge/w5/prebuilt/etc/thermal-engine-8610.conf:system/etc/thermal-engine-8610.conf \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
