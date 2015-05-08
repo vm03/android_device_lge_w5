3ed642b911ca1adfe50d3e1e7d7b191c76595a49
vm03 <vm03@yandex.ru>
2015-05-08 09:02:53 +0300

    fixup keys for f8
    Change-Id: I18da3d0b7ab97d643cf42a2df2cc310700282ce3

--- prebuilt/fixup.sh
+++ prebuilt/fixup.sh
@@ -21,12 +21,33 @@ case $model in
 	rm -rf /system/app/NfcNci
 	cat /system/usr/keylayout/Generic-D325.kl > /system/usr/keylayout/Generic.kl
           ;; 
+     "LG-D325f8")
+	rm /system/etc/permissions/android.hardware.nfc.xml
+	rm /system/etc/permissions/android.hardware.nfc.hce.xml
+	rm /system/lib/hw/nfc_nci.w5.so
+	rm -rf /system/app/NfcNci
+	cat /system/usr/keylayout/Generic-D325.kl > /system/usr/keylayout/Generic.kl
+          ;;
+     "LG-D320f8")
+	rm /system/etc/permissions/android.hardware.nfc.xml
+	rm /system/etc/permissions/android.hardware.nfc.hce.xml
+	rm /system/lib/hw/nfc_nci.w5.so
+	rm -rf /system/app/NfcNci
+	cat /system/usr/keylayout/Generic-D320.kl > /system/usr/keylayout/Generic.kl
+	;;
      "LG-D325F8")
 	rm /system/etc/permissions/android.hardware.nfc.xml
 	rm /system/etc/permissions/android.hardware.nfc.hce.xml
 	rm /system/lib/hw/nfc_nci.w5.so
 	rm -rf /system/app/NfcNci
 	cat /system/usr/keylayout/Generic-D325.kl > /system/usr/keylayout/Generic.kl
+          ;;
+     "LG-D320F8")
+	rm /system/etc/permissions/android.hardware.nfc.xml
+	rm /system/etc/permissions/android.hardware.nfc.hce.xml
+	rm /system/lib/hw/nfc_nci.w5.so
+	rm -rf /system/app/NfcNci
+	cat /system/usr/keylayout/Generic-D320.kl > /system/usr/keylayout/Generic.kl
           ;;
      "LGMS323")
 	rm /system/etc/permissions/android.hardware.nfc.xml
